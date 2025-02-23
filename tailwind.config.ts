b2e2260f4c7cf98248c6f9ced0a5cd1ff47e3bd4
solmee <kii5541@naver.com>
2025-02-24 02:04:13 +0900

    setting: Gyeonggi, Pretendard 폰트 적용
    - font-pretendard, font-gyeonggi 클래스로 사용 가능하도록 tailwind.config.ts에 폰트 설정 추가
    - globals.css에 @font-face 웹 폰트 추가
    - layout.tsx에 기본 폰트 제거

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,35 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
-export default {
+const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+      fontFamily: {
+        gyeonggi: ['Gyeonggi', 'sans-serif'],
+        pretendard: ['Pretendard', 'sans-serif'],
+      },
+      fontWeight: {
+        thin: '100',
+        extralight: '200',
+        light: '300',
+        normal: '400',
+        medium: '500',
+        semibold: '600',
+        bold: '700',
+        extrabold: '800',
+        black: '900',
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
+
+export default config;
